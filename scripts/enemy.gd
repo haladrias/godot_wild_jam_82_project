5f2cf180d096225f7e0b7877f1e7fd3a5f0a3026
allen-chi <58610600+allen-chi@users.noreply.github.com>
2025-06-19 11:12:22 -0700

    Updating files

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,25 +1,23 @@
-extends CharacterBody2D
-@export var input_component: InputComponent
-@export var movement_component: MovementComponent
-@export var detection_component: DetectionComponent
-@onready var debug_label: RichTextLabel = $DebugLabel
-@onready var entity: CollisionShape2D = $CollisionShape2D
+class_name Enemy extends Entity
+@export var pathfinder: Pathfinder
+
 
 func _ready() -> void:
 	#detection_component.proximity_detection_triggered.connect(panic)
-	detection_component.view_cone_detection_triggered.connect(panic)
+	detection_component.view_cone_detection_triggered.connect(detected)
+	detection_component.view_cone_detection_stopped.connect(not_detected)
+	DebugTools.update_debug_label(debug_label, "I don't see anything")
+	SignalBus.debug_spacebar_pressed.connect(debug_movement)
 	pass
 
 func _process(_delta):
-	#detection_component.set_view_cone_rotation(input_component.get_mouse_location())
-	#entity.look_at(input_component.get_mouse_location())
-	#move_and_slide()
+	# TODO: parameters to pass into handle_movement should be self, and some other direction as determined by pathfinding
+	#movement_component.handle_movement(self, input_component.get_input_direction())
+	# move_and_slide()
+	#pathfinder.update_path(vector2i_position())
+	pathfinder.update_entity_position(global_position)
+	pathfinder.update_path()
 	return
 
-func panic():
-	DebugTools.update_debug_label(debug_label, "I've been seen!")
-
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("PlayerDetector"):
-		panic()
+func debug_movement():
+	self.position.x += 50
