49b00ec2c011a701e06eb926650ad77922a8bb7e
allen-chi <58610600+allen-chi@users.noreply.github.com>
2025-06-18 22:32:22 -0700

    Revert "Update to main"
    This reverts commit 763afa4ef2412bf0aae3ed54cc63f15802482a70.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,1 @@
 extends Node2D
-
-func _ready() -> void:
-	SignalBus.debug_set_grid_size.connect(set_panel)
-
-# For debug, we shoould remove it once we have a tilemap in place
-func set_panel(size):
-	$Panel.size = size
-	print("The playable area is " + str($Panel.size) + ". Remain within the panel boundary for the pathing to work.")
