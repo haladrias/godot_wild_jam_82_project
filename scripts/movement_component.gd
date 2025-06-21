4b3c7af28eeb919e630ec887fa9c92e1b0d1bbdc
haladrias <haladrias@gmail.com>
2025-06-21 15:27:23 -0700

    Revert "Merge branch 'main' into power_system"
    This reverts commit 855d7a6ec6af9f9a3163095a0e426cfd75ed04cb, reversing
    changes made to c77fd7672125df76a731886ad378d1e057bac5e5.

--- scripts/movement_component.gd
+++ scripts/movement_component.gd
@@ -2,67 +2,8 @@ extends Node2D
 class_name MovementComponent
 @export_subgroup("Movement Settings")
 @export var speed: int = 400
-@export var amt_to_slow : int = 200.0
-@export var parent : CharacterBody2D
 
-
-func _ready() -> void:
-	AilmentManager.entered.connect(apply_status_effect)
-	AilmentManager.exited.connect(remove_status_effect)
 
 func handle_movement(body: CharacterBody2D, direction: Vector2):
 	body.velocity = direction.normalized() * speed
 	direction = body.velocity
-
-
-func apply_status_effect()-> void:
-	#if AilmentManager.ailment == 1:
-		#_slow()
-	#elif AilmentManager.ailment == 2:
-		#_stun()
-		#remove_status_effect()
-
-	match AilmentManager.target:
-		1:#PLAYER
-			if parent.is_in_group("Player"):
-				if AilmentManager.ailment == 1:
-					_slow()
-				elif AilmentManager.ailment == 2:
-					_stun()
-					remove_status_effect()
-		2:#ENEMY
-			if parent.is_in_group("Enemy"):
-				if AilmentManager.ailment == 1:
-					_slow()
-				elif AilmentManager.ailment == 2:
-					_stun()
-					remove_status_effect()
-		_:
-			print("target out of scope")
-
-
-func remove_status_effect() -> void:
-	if AilmentManager.ailment == 1:
-		speed = 400
-	elif AilmentManager.ailment == 2:
-		await get_tree().create_timer(3).timeout
-		speed = 400
-		for c in parent.get_children():
-			if c is CollisionShape2D:
-				c.disabled = false
-				print("enabled ailment")
-
-func _slow() -> void:
-	speed -= amt_to_slow
-	pass
-
-func _stun() -> void:
-	await get_tree().create_timer(0.5).timeout
-	speed = 0
-
-	for c in parent.get_children():
-		if c is CollisionShape2D:
-			c.disabled = true
-			print("disabled")
-
-	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,5 @@
 class_name Player extends Entity
 
-func _ready() -> void:
-	#detection_component.proximity_detection_triggered.connect(panic)
-	detection_component.view_cone_detection_triggered.connect(detected)
-	detection_component.view_cone_detection_stopped.connect(not_detected)
-	DebugTools.update_debug_label(debug_label, "I don't see anything")
-	pass
-
 func _physics_process(_delta):
 	movement_component.handle_movement(self, input_component.get_input_direction())
 	move_and_slide()
