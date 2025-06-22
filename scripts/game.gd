25b56da8377168d7c981f1b697f7044b4be68ed1
sadel-V <inflateship@gmail.com>
2025-06-22 11:32:30 +0800

    added_audio_in_game

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
+
+
 func _ready() -> void:
 	SignalBus.debug_set_grid_size.connect(set_panel)
+	audio_player.play()
+	PauseMenu.music_stop.connect(background_music_stop)
+	PauseMenu.music_playing.connect(background_music_play)
 
 # For debug, we shoould remove it once we have a tilemap in place
 func set_panel(size):
 	$Panel.size = size
 	print("The playable area is " + str($Panel.size) + ". Remain within the panel boundary for the pathing to work.")
+
+func background_music_stop() -> void:
+	if PauseMenu.is_playing == false:
+		audio_player.stop()
+		print("method called")
+	pass
+
+func background_music_play() -> void:
+	if PauseMenu.is_playing == true:
+		audio_player.play()
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,15 +1,22 @@
 extends CanvasLayer
 
+signal music_playing
+signal music_stop
+
 @onready var resume: Button = $Control/CenterContainer/VBoxContainer/resume
 @onready var settings: Button = $Control/CenterContainer/VBoxContainer/settings
 @onready var quit_to_title: Button = $"Control/CenterContainer/VBoxContainer/quit to title"
+@onready var bgm_toggle: CheckBox = $Control/CenterContainer2/PanelContainer/GridContainer/bgm_toggle
+
 
 var is_paused : bool = false
+var is_playing : bool = true
 
 func _ready() -> void:
 	hide_pause_menu()
 	resume.pressed.connect(hide_pause_menu)
 	quit_to_title.pressed.connect(on_quit_pressed)
+	bgm_toggle.pressed.connect(control_bgm)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
@@ -35,3 +42,13 @@ func on_quit_pressed() -> void:
 	await get_tree().process_frame
 	
 	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
+func control_bgm():
+	if is_playing == true:
+		is_playing = false
+		music_stop.emit()
+	elif is_playing == false:
+		is_playing = true
+		music_playing.emit()
+		
+	pass
